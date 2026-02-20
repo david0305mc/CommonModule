e933b7eeb2fb2fa378077123ccab6128ebdca213
david0305 <david0305mc@gmail.com>
2026-02-20 17:45:44 +0900

    TestCode

---
+++ Assets/Test/UtilTest/UtilTest.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UtilTest : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    void Awake()
+    {
+        button.onClick.AddListener(() =>
+        {
+            Debug.Log($"MathUtil.Clamp01(100f) {MathUtil.Clamp01(100f)}");
+        });
+    }
+}
