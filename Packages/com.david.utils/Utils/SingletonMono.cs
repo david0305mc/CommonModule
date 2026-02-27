5dd3c97a38819c7307da78832744aac434192bf4
david0305 <david0305mc@gmail.com>
2026-02-27 17:06:04 +0900

    코드 개선

--- Packages/com.david.utils/Utils/SingletonMono.cs
+++ Packages/com.david.utils/Utils/SingletonMono.cs
@@ -3,18 +3,24 @@ using UnityEngine;
 public abstract class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
-
-    // Awake 기반 초기화가 끝났는지 (Awake보다 먼저 Instance가 호출되는 케이스 방어)
     private static bool initialized;
 
-    public static bool HasInstance => instance != null && !instance.Equals(null);
+    public static bool HasInstance
+    {
+        get
+        {
+            CleanupIfDestroyed();
+            return instance != null;
+        }
+    }
 
     public static T Instance
     {
         get
         {
-            // Unity 오브젝트는 파괴돼도 C# 참조가 남을 수 있어서 Equals(null) 방어가 필요
-            if (instance != null && !instance.Equals(null))
+            CleanupIfDestroyed();
+
+            if (instance != null)
                 return instance;
 
             instance = FindSingletonInScene();
@@ -26,10 +32,8 @@ public abstract class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
                 return null;
             }
 
-            // 아직 Awake 초기화가 안 된 상태에서 Instance가 먼저 불린 경우 경고 (선택)
             if (!initialized)
             {
-                // 이 경고가 시끄럽다면 제거 가능
                 Debug.LogWarning($"[SingletonMono] {typeof(T).Name} Instance가 Awake 이전에 호출됨. " +
                                  $"Awake에서 초기화되는 값에 의존하면 순서 이슈가 날 수 있어.");
             }
@@ -40,17 +44,11 @@ public abstract class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
 
     [SerializeField] protected bool dontDestroyOnLoad = false;
 
-    // 플레이 시작 시 static 초기화 (도메인 리로드 옵션 꺼져 있어도 안전)
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-    private static void ResetStatics()
-    {
-        instance = null;
-        initialized = false;
-    }
-
     protected virtual void Awake()
     {
-        if (instance != null && !instance.Equals(null) && instance != this)
+        CleanupIfDestroyed();
+
+        if (instance != null && instance != this)
         {
             Destroy(gameObject);
             return;
@@ -67,21 +65,32 @@ public abstract class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
 
     protected virtual void OnDestroy()
     {
-        if (instance == this) instance = null;
+        if (instance == this)
+        {
+            instance = null;
+            initialized = false;
+        }
     }
 
     protected virtual void OnInitialize() { }
 
+    private static void CleanupIfDestroyed()
+    {
+        // UnityEngine.Object는 Destroy되면 C# 참조가 남아도 "== null"처럼 동작할 수 있어서 방어 필요
+        if (instance != null && instance.Equals(null))
+        {
+            instance = null;
+            initialized = false;
+        }
+    }
+
     private static T FindSingletonInScene()
     {
 #if UNITY_2023_1_OR_NEWER
-        // 2023.1+ : 비활성 오브젝트 포함 검색 가능
         return Object.FindFirstObjectByType<T>(FindObjectsInactive.Include);
 #elif UNITY_2022_2_OR_NEWER
-        // 일부 2022 LTS에서도 지원되는 케이스가 있음(프로젝트 설정/버전에 따라 다름)
         return Object.FindFirstObjectByType<T>(FindObjectsInactive.Include);
 #else
-        // 구버전: 활성 오브젝트만 검색
         return Object.FindObjectOfType<T>();
 #endif
     }
