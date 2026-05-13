dc54d6b39e35c91105a48af80e81346ff85dafff
david0305 <david0305mc@gmail.com>
2026-05-13 18:57:59 +0900

    Test - HFSM Player

---
+++ Assets/Test/HFSMSample/HFSMTest2D/PlayerObj.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace HFSMTest2D
+{
+    public class PlayerObj : MonoBehaviour
+    {
+        public static PlayerObj Instance { get; private set; }
+        void Start()
+        {
+            Instance = this;
+        }
+
+        void Update()
+        {
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            input = Vector2.ClampMagnitude(input, 1);
+            input *= 5;
+
+            transform.position += (Vector3)(input * Time.deltaTime);
+        }
+    }
+
+}
